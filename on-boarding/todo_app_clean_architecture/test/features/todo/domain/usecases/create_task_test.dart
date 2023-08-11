8a2ae141a5e6a875006dae6ad9e73db2375d39ad
betabravahC:/Users/betty/AppData/Local/Programs/Python/Python310/python.exe <betabravah@gmail.com>
2023-08-12 02:06:16 +0300

    Add Todo Clean Architecture

---
+++ on-boarding/todo_app_clean_architecture/test/features/todo/domain/usecases/create_task_test.dart
@@ -0,0 +1,37 @@
+import 'package:dartz/dartz.dart' hide Task;
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:todo_app_clean_architecture/features/todo/domain/repositories/task_repository.dart';
+import 'package:todo_app_clean_architecture/features/todo/domain/usecases/create_task.dart';
+import 'package:todo_app_clean_architecture/features/todo/domain/entities/task.dart';
+
+class MockTaskRepository extends Mock implements TaskRepository {}
+
+void main() {
+  late CreateTask usecase;
+  late MockTaskRepository mockTaskRepository;
+
+  setUp(() {
+    mockTaskRepository = MockTaskRepository();
+    usecase = CreateTask(mockTaskRepository);
+  });
+
+  const tTaskId = 1;
+  final tTask = Task(
+    id: tTaskId,
+    title: "Test Title",
+    description: "Test Description",
+    dueDate: DateTime.now(),
+  );
+
+  test('should create task to repository', () async {
+    when(mockTaskRepository.createTask(tTask))
+        .thenAnswer((_) async => Right(tTask));
+
+    final result = await usecase(Params(task: tTask));
+
+    expect(result, Right(tTask));
+    verify(mockTaskRepository.createTask(tTask));
+    verifyNoMoreInteractions(mockTaskRepository);
+  });
+}
